690c2c3185cbd5bd688cf79cf9629db33aa7259e
Chloe Pinrip <pinrip@null.null>
2026-07-16 22:13:40 +0000

    build: Add top-level build driver
    Signed-off-by: Chloe Pinrip <pinrip@null.null>

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#
+# Copyright (c) 2026, pinrip, rixel, et al.
+#
+# Abstract:
+#       Top-level build driver
+# Author:
+#       Chloe Pinrip
+#
+
+MAKE=make
+
+#
+# Check if a list of deps are installed
+#
+# $@:  List of deps to check
+#
+check_deps() {
+    for dep in $@
+    do
+        printf "Checking if $dep is installed... "
+        which $dep &>/dev/null
+        if [ $? -ne 0 ]
+        then
+            echo "no"
+            echo "Please install '$dep'!"
+            exit 1
+        fi
+
+        echo "yes"
+    done
+}
+
+#
+# <$1>: Build target
+# <$2>: Build message
+#
+make_build() {
+    echo -e "[*] $2"
+    pushd $1;   \
+        $MAKE
+    popd
+}
+
+#
+# Begin the build process
+#
+build() {
+    mkdir -p artifacts/
+
+    make_build          \
+        service/ntos    \
+        "Building kernel..."
+
+    make_build          \
+        service/boot/   \
+        "Building bootloader..."
+}
+
+# Verify all deps
+check_deps  \
+    clang   \
+    mtools  \
+    rsync
+
+# Begin the build process
+build
