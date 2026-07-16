0511eb0afbaed09adc8fcba075baf8a1f5096daf
Chloe Pinrip <pinrip@null.null>
2026-07-16 23:43:22 +0000

    tools: Add .img generation utility
    Signed-off-by: Chloe Pinrip <pinrip@null.null>

---
+++ tools/gen-img.sh
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2026, pinrip, rixel, et al.
+#
+# Abstract:
+# 	    Generate an image from the boot files
+# Author:
+# 	    Chloe Pinrip
+#
+
+EFI_BOOT=artifacts/BOOTX64.EFI
+KERNEL=artifacts/ntoskrnl.sys
+IMG_NAME=artifacts/vino.img
+
+dd if=/dev/zero of=$IMG_NAME bs=1k count=1440
+mformat -i $IMG_NAME -f 1440 ::
+mmd -i $IMG_NAME ::/EFI
+mmd -i $IMG_NAME ::/EFI/BOOT
+mcopy -i $IMG_NAME $KERNEL ::
+mcopy -i $IMG_NAME $EFI_BOOT ::/EFI/BOOT
